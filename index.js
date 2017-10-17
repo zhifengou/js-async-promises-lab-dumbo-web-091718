80018e1b6c0391120ca566a370e2229801ae692e
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2017-10-17 14:27:35 -0400

    update index.js

--- index.js
+++ index.js
@@ -5,3 +5,55 @@ const questions = [
 ]
 
 let question;
+function askQuestion(){
+  return questions[0]
+}
+
+function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+
+function trueAndFalseButtons(){
+  return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+function checkQuestion(question, answer){
+  question.questionAnswer == answer
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+
+function appendQuestion(question){
+  let container = document.querySelector('.question-container')
+  container.innerHTML = question.questionText;
+}
+
+function removeQuestion(){
+  return new Promise(function(){
+    let container = document.querySelector('.question-container')
+    container.innerHTML = ''
+    toggleTrueAndFalseButtons()
+  })
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
