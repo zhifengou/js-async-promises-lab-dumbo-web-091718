01b3231c374855f8018d9869432dfb1bdbd92393
zhifeng ou <zhifengou@hotmail.com>
2019-03-15 19:58:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ const questions = [
 ]
 
 let question;
+function trueAndFalseButtons(){
+  btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+  return btns;
+}
+ function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+ function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+ function appendQuestion(question) {
+  const questionContainer = document.getElementById("question")
+  questionContainer.textContent = question.questionText
+}
+ function askQuestionThen(time) {
+  question = questions[0];
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+ function removeQuestion() {
+  return new Promise(function(){
+    const questionContainer = document.getElementById("question")
+    questionContainer.textContent = ""
+    toggleTrueAndFalseButtons()
+  })
+}
+ function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time).then(removeQuestion)
+}
